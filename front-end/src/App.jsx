8c3bb6729dd83d71a64c281eddabbc9fde54bad6
BekCodingAddict <experte001@gmail.com>
2024-05-14 18:32:55 +0900

    UI has been designed and ffake API has been connected!

---
+++ front-end/src/App.jsx
@@ -0,0 +1,17 @@
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
